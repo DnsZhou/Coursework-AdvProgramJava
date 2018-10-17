ccc46cc650b919316d8376a13ab90feaa7c1ba02
Dennis Zhou <zt1020@yeah.net>
2018-10-17 03:39:27 +0100

    finished partial of the car registration number, unitest unfinished.

--- src/uk/ac/ncl/TongZhou/advProgmJava/Entity/CarRegistrationNumber.java
+++ src/uk/ac/ncl/TongZhou/advProgmJava/Entity/CarRegistrationNumber.java
@@ -6,14 +6,58 @@
  */
 package uk.ac.ncl.TongZhou.advProgmJava.Entity;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.params.converter.ArgumentConversionException;
 
 /**
- * @ClassName: CarRegistrationNumber 
- * @Description: A car registration number has two components. The first component is two letters followed by two 
- * digits. The second component is three letters. Registration numbers are unique. No two cars have the same 
- * registration number.
+ * @ClassName: CarRegistrationNumber
+ * @Description: A car registration number has two components. The first
+ *               component is two letters followed by two digits. The second
+ *               component is three letters. Registration numbers are unique. No
+ *               two cars have the same registration number.
  * 
  */
-public class CarRegistrationNumber {
+public final class CarRegistrationNumber {
+	private static Map<String, CarRegistrationNumber> allCarRegistrationNumbers;
+	private final String comp1;
+	private final String comp2;
 
+	private CarRegistrationNumber(String comp1, String comp2) {
+		this.comp1 = comp1;
+		this.comp2 = comp2;
+	}
+
+	public CarRegistrationNumber getInstance(String carRegistrationNumber) throws IllegalArgumentException {
+		if (allCarRegistrationNumbers == null)
+			allCarRegistrationNumbers = new HashMap<String, CarRegistrationNumber>();
+
+		if (carRegistrationNumber == null)
+			throw new IllegalArgumentException("carRegistrationNumber should not be null.");
+
+		String carRegistrationNumbertrim = carRegistrationNumber.trim();
+		if (!validateCarRegNumber(carRegistrationNumbertrim))
+			throw new IllegalArgumentException("carRegistrationNumber : argument format not validated");
+
+		String comp1 = carRegistrationNumbertrim.substring(0, 4);
+		String comp2 = carRegistrationNumbertrim.substring(carRegistrationNumbertrim.length() - 3);
+		String key = comp1 + comp2;
+
+		CarRegistrationNumber crn = allCarRegistrationNumbers.get(key);
+		if (crn == null) {
+			crn = new CarRegistrationNumber(comp1, comp2);
+			allCarRegistrationNumbers.put(key, crn);
+		} else {
+			
+		}
+		return crn;
+	}
+
+	private boolean validateCarRegNumber(String carRegistrationNumber) {
+		Pattern p = Pattern.compile("\\[A-Za-z]{2}\\d{2}\\s*\\[A-Za-z]{3}");
+		return p.matcher(carRegistrationNumber).matches();
+	}
 }
